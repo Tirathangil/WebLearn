5e36ff666328cb7913d202ae7211accfccd29114
Tirathangil <tirathangil@yandex.ru>
2020-11-12 16:31:48 +0300

    Refactored bubbles.js

--- JS_Bubbles/js/bubbles.js
+++ JS_Bubbles/js/bubbles.js
@@ -1,26 +1,47 @@
-BubblesData = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 75, 30, 39, 75, 21, 33, 75];
-ScoresData  = [.25, .32, .14, .48, .25, .47, .34, .16, .47, .49, .57, .65, .24, .16, .79, .48, .54]
-var i = 0;
-var j = 0;
+var BubblesData = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 75, 30, 39, 75, 21, 33, 75];
+var ScoresData = [0.25, 0.32, 0.14, 0.48, 0.25, 0.47, 0.34, 0.16, 0.47, 0.49, 0.57, 0.65, 0.24, 0.16, 0.79, 0.48, 0.54];
 var MaxBubbles = 0;
 var MaxBubblesElements = [];
+var OptimalScoreElement = 0;
+
+function WriteExperimentsResults(Exp) {
+    for (var i = 0; i < Exp.length; i++) {
+        document.write("Эксперимент №" + (i + 1) + ". Пузырей: " + BubblesData[i] + " Стоимость: " + ScoresData[i] + "<br>");
+        if (MaxBubbles < Exp[i]) {
+            MaxBubbles = Exp[i];
+        }
+    }
+}
+
+function GetMaxBubbles(Exp) {
+    var MaxElements = [];
+    for (var j = 0; j < (Exp.length); j++) {
+        if (Exp[j] == MaxBubbles) {
+            MaxElements.push(++j);
+        }
+    }
+    return MaxElements;
+}
+
+function GetOptimalScore(Scores, MaxElements) {
+    var MaxScore = 1;
+
+    for (var i = 0; i < MaxElements.length; i++) {
+        if (Scores[MaxElements[i]] < MaxScore) {
+            MaxScore = MaxElements[i];
+        }
+    }
+
+    return MaxScore;
+}
+
 document.write("<h1>Отчет по экспериментам с пузырями</h1><br>");
-for (i=0; i<BubblesData.length; i++) {
-    document.write("Эксперимент №" + (i + 1) + ". Пузырей: " + BubblesData[i] + "<br>");
-    if (MaxBubbles < BubblesData[i]) {
-        MaxBubbles = BubblesData[i];
-    }
-}
-
-for (j = 0; j < (BubblesData.length); j++) {
-    if (BubblesData[j] == MaxBubbles) {
-        MaxBubblesElements.push(++j);
-    }
-}
-
+WriteExperimentsResults(BubblesData);
 document.write("<strong>Всего тестов: </strong>" + BubblesData.length + "<br>");
 document.write("<strong>Наивысший результат: </strong>" + MaxBubbles + "<br>");
+MaxBubblesElements = GetMaxBubbles(BubblesData);
 document.write("<strong>Элементы с наив. результатом: </strong>");
 for (i = 0; i < MaxBubblesElements.length; i++) {
-    document.write(MaxBubblesElements[i]+", ");
+    document.write(MaxBubblesElements[i] + ", ");
 }
+document.write("<strong>Оптимальный элемент: </strong> №" + GetOptimalScore(ScoresData, MaxBubblesElements));
